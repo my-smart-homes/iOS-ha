bfe78b265eda9d5d23c1b7014801cb43f4078f9f
Naimur <naimurhasanrwd@gmail.com>
2025-02-15 13:15:35 +0600

    fix: notification url

--- Sources/Shared/API/HAAPI.swift
+++ Sources/Shared/API/HAAPI.swift
@@ -452,7 +452,7 @@ public class HomeAssistantAPI {
         with(MobileAppRegistrationRequest()) {
             if let pushID = Current.settingsStore.pushID {
                 $0.AppData = [
-                    "push_url": "https://mobile-apps.home-assistant.io/api/sendPushNotification",
+                    "push_url": "https://us-central1-fourth-return-421315.cloudfunctions.net/sendPushNotification",
                     "push_token": pushID,
                 ]
             }
